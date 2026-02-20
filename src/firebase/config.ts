dcd1a32eeac902b9d69573e5442b8953202adf77
Srivaastava G <srivastava.chari@gmail.com>
2026-02-20 05:19:23 +0000

    Firebase storage : no default bucket found

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:396174042144:web:7b68e93e4aa93667280845",
   "apiKey": "AIzaSyDvTNzHmuBtWLYn5l_UZT1K23HxCwy0cUY",
   "authDomain": "studio-7911749699-76366.firebaseapp.com",
+  "storageBucket": "studio-7911749699-76366.appspot.com",
   "measurementId": "",
   "messagingSenderId": "396174042144"
 };
