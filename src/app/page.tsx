4dc0aa2d2e4d087ce123c51ae4c15e2818c8d405
Srivaastava G <srivastava.chari@gmail.com>
2026-02-22 16:26:45 +0000

    Use this attached image for the top section of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -135,8 +135,8 @@ export default function Home() {
       <main className="flex-1">
         <section className="relative flex h-[85vh] flex-col items-center justify-center text-white">
             <Image
-                src="https://images.unsplash.com/photo-1554104707-a4b67023c530?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHx2aWV3JTIwZnJvbSUyMGpldCUyMHdpbmRvd3xlbnwwfHx8fDE3NzE4ODU5NzN8MA&ixlib=rb-4.1.0&q=80&w=1080"
-                alt="Private jet wing soaring above clouds at sunset"
+                src="https://storage.googleapis.com/project-spark-335111-public/a927a7c7-deec-4001-8b32-0c9398ac38d7"
+                alt="Private jet flying over a mountain range at sunset"
                 fill
                 className="object-cover"
                 priority
