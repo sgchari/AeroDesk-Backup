d8f77183ad3514daef475e03173b39a8136af396
Srivaastava G <srivastava.chari@gmail.com>
2026-02-23 16:23:27 +0000

    Again the image is replaced,  can you try this attached image instead of

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -248,7 +248,7 @@ export default function Home() {
       <div
         className="fixed inset-0 z-0 bg-cover bg-center"
         style={{
-          backgroundImage: "url('https://images.unsplash.com/photo-1488085061387-422e29b40080?q=80&w=2070&auto=format&fit=crop')",
+          backgroundImage: "url('https://images.unsplash.com/photo-1627440474139-65a5d1656f7e?q=80&w=2070&auto=format&fit=crop')",
         }}
       >
         <div className="absolute inset-0 bg-black/30" />
