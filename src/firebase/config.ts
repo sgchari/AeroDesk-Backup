b48d586012fa40224e1054af1c52989574201791
Srivaastava G <srivastava.chari@gmail.com>
2026-02-25 04:34:37 +0000

    As marked in the attached screenshot ,  we are currently working on aero

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-7911749699-76366",
+  "projectId": "aerodeskbackup-04379076-63979",
   "appId": "1:396174042144:web:7b68e93e4aa93667280845",
   "apiKey": "AIzaSyDvTNzHmuBtWLYn5l_UZT1K23HxCwy0cUY",
-  "authDomain": "studio-7911749699-76366.firebaseapp.com",
-  "storageBucket": "studio-7911749699-76366.appspot.com",
+  "authDomain": "aerodeskbackup-04379076-63979.firebaseapp.com",
+  "storageBucket": "aerodeskbackup-04379076-63979.appspot.com",
   "measurementId": "",
   "messagingSenderId": "396174042144"
 };
