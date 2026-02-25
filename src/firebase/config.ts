2e7b787a2a694c55317eef291e80ab10ed6daf53
Srivaastava G <srivastava.chari@gmail.com>
2026-02-25 04:35:07 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
   "projectId": "aerodeskbackup-04379076-63979",
-  "appId": "1:396174042144:web:7b68e93e4aa93667280845",
-  "apiKey": "AIzaSyDvTNzHmuBtWLYn5l_UZT1K23HxCwy0cUY",
+  "appId": "1:774524736627:web:fed240633330babc250606",
+  "apiKey": "AIzaSyCT7aClvSUjDzxJLmLtxqLsGsyUX_Edkpg",
   "authDomain": "aerodeskbackup-04379076-63979.firebaseapp.com",
   "storageBucket": "aerodeskbackup-04379076-63979.appspot.com",
   "measurementId": "",
-  "messagingSenderId": "396174042144"
+  "messagingSenderId": "774524736627"
 };
